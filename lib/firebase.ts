d2c79ffac0243f6e7f6c97fa63b7cb1aa640035f
MK MUZAKKIR <muzakkir7977@gmail.com>
2025-06-27 15:00:37 +0530

    sdf

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,19 +5,25 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey:              process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain:          process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId:           process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket:       process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:   process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId:               process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId:       process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL:         process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyDdvjIZI-BBBubiE_B6f29tFcOGek3MY4Y",
+  authDomain: "medford-e07a1.firebaseapp.com",
+  projectId: "medford-e07a1",
+  storageBucket: "medford-e07a1.firebasestorage.app",
+  messagingSenderId: "15585322243",
+  appId: "1:15585322243:web:8aaaab45f2345847373536",
+  measurementId: "G-SR1NMTGT1T"
 };
 
-if (!firebaseConfig.apiKey) {
-  throw new Error("Missing Firebase configuration in .env.local");
-}
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDOlJU3qctCx_77DdhqDg-T9IvUGkMV1LU",
+//   authDomain: "face-recognition-d6dcd.firebaseapp.com",
+//   databaseURL: "https://face-recognition-d6dcd-default-rtdb.firebaseio.com",
+//   projectId: "face-recognition-d6dcd",
+//   storageBucket: "face-recognition-d6dcd.firebasestorage.app",
+//   messagingSenderId: "585222259083",
+//   appId: "1:585222259083:web:e5a09bf166a9651b262f8e",
+//   measurementId: "G-SQ0ZLK8ZRX"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -26,4 +32,4 @@ const provider = new GoogleAuthProvider();
 const db = getDatabase(app);
 const storage = getStorage(app);
 
-export { auth, provider, signInWithPopup, signOut, db, storage };
+export { auth, provider, signInWithPopup, signOut ,db , storage };
